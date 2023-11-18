a7d0009eeb7e6866e25980b534cfb6fc57135567
Junwon Lee <63298243+cpprhtn@users.noreply.github.com>
2023-11-19 01:19:10 +0900

    Update carbon_track.py

--- carbon_track.py
+++ carbon_track.py
@@ -6,17 +6,6 @@ from carbon_api import carbon_intensity
 from firebase import FirebaseManager
 from firebase_admin import db
 import json
-import hashlib
-
-def encrypt_number(number):
-    # 숫자를 문자열로 변환하여 바이트로 인코딩
-    number_str = str(number)
-    number_bytes = number_str.encode('utf-8')
-
-    # SHA-256 해시 계산
-    sha256_hash = hashlib.sha256(number_bytes).hexdigest()
-
-    return sha256_hash
 
 class CarbonTrack:
     def __init__(self):
