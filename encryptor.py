f6d31398bec5659f1732fbefc4fe9180c8388a40
Junwon Lee <63298243+cpprhtn@users.noreply.github.com>
2023-11-19 01:16:57 +0900

    Update encryptor.py

--- encryptor.py
+++ encryptor.py
@@ -5,12 +5,15 @@ class NumberEncryptor:
         
         pass
 
-    def encrypt_number(self, number):
+    def encrypt_number(self, input_data):
         # 숫자를 문자열로 변환하여 바이트로 인코딩
-        number_str = str(number)
-        number_bytes = number_str.encode('utf-8')
+        if isinstance(input_data, tuple):
+            input_data = ''.join(map(str, input_data))
+
+        # 문자열을 바이트로 인코딩
+        input_bytes = input_data.encode('utf-8')
 
         # SHA-256 해시 계산
-        sha256_hash = hashlib.sha256(number_bytes).hexdigest()
+        sha256_hash = hashlib.sha256(input_bytes).hexdigest()
 
         return sha256_hash
