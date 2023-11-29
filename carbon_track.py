79025879727effc15764f3af1372255e9764e93a
cpprhtn <cpprhtn@naver.com>
2023-11-29 18:09:04 +0900

    add: concat m1_gpu

--- carbon_track.py
+++ carbon_track.py
@@ -8,13 +8,21 @@ from firebase_admin import db
 import json
 from gpu import GPUInfo
 # from datetime import datetime
+import platform
+import time
 
 class CarbonTrack:
     def __init__(self):
         self.fm = FirebaseManager() # firebase 연결 완료
         self.gpu_info = GPUInfo(gpu_id=0)
-        self.memory_usage = self.gpu_info.get_gpu_memory_usage()
-        self.power_usage = self.gpu_info.get_gpu_power_usage()
+        if platform.machine() == "aarch64" or platform.machine() == "arm64":
+            self.gpu_info.gpu_name = "macOS M1 GPU"
+            # self.gpu_info.gpu_id = "macOS M1 GPU"
+            self.memory_usage, self.power_usage = self.gpu_info.get_gpu_m1()
+
+        else:
+            self.memory_usage = self.gpu_info.get_gpu_memory_usage()
+            self.power_usage = self.gpu_info.get_gpu_power_usage()
 
         
 
@@ -22,7 +30,7 @@ class CarbonTrack:
         self.carbon_intensity = json.loads(json.dumps(carbon_intensity(zone='KR', data='intensity', format='history')))
         print("run __init__ part")
         print(f"GPU Name: {self.gpu_info.gpu_name}")
-        print(f"GPU {self.gpu_info.gpu_id}의 메모리 사용량: {self.memory_usage} MiB")
+        print(f"GPU {self.gpu_info.gpu_id}의 메모리 사용량: {self.memory_usage} %")
         print(f"GPU {self.gpu_info.gpu_id}의 전력 사용량: {self.power_usage} kWh")
         data = {'carbon_intensity': self.carbon_intensity['carbon_intensity'], 'gpu_info': self.gpu_info.gpu_name, 'memory_usage': self.memory_usage, 'power_usage': self.power_usage}
         self.gpu = json.loads(json.dumps(data))
@@ -30,10 +38,21 @@ class CarbonTrack:
         self.fm.update(self.gpu)
 
     def collect(self):
-        self.memory_usage = self.gpu_info.get_gpu_memory_usage()
-        self.power_usage = self.gpu_info.get_gpu_power_usage()
+        if platform.machine() == "aarch64" or platform.machine() == "arm64":
+            self.memory_usage, self.power_usage = self.gpu_info.get_gpu_m1()
+
+        else:
+            self.memory_usage = self.gpu_info.get_gpu_memory_usage()
+            self.power_usage = self.gpu_info.get_gpu_power_usage()
+
         self.carbon_intensity = json.loads(json.dumps(carbon_intensity(zone='KR', data='intensity', format='history')))
         data = {'carbon_intensity': self.carbon_intensity['carbon_intensity'], 'gpu_info': self.gpu_info.gpu_name, 'memory_usage': self.memory_usage, 'power_usage': self.power_usage}
         self.gpu = json.loads(json.dumps(data))
         # self.fm.post(str(datetime.now().strftime('%Y%m%d%H%M%S')), self.gpu)
         self.fm.update(self.gpu)
+
+if __name__ == "__main__":
+    ct = CarbonTrack()
+    while True:
+        ct.collect()
+        time.sleep(60) # 1분마다 데이터 수집
