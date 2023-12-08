b2b91a7f034ed6918b9af40924600942921c854f
DaeRo <80153046+dhfgoeofh@users.noreply.github.com>
2023-12-08 17:42:10 +0900

    vggmodel_update

---
+++ vgg_model/vggmodel.py
@@ -0,0 +1,62 @@
+import torch
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import json
+import os
+
+
+class VGGNet(nn.Module):
+    def __init__(self):
+        super(VGGNet, self).__init__()
+        self.conv = nn.Sequential(
+            #3 224 128
+            nn.Conv2d(3, 64, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.Conv2d(64, 64, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.MaxPool2d(2, 2),
+            #64 112 64
+            nn.Conv2d(64, 128, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.Conv2d(128, 128, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.MaxPool2d(2, 2),
+            #128 56 32
+            nn.Conv2d(128, 256, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.Conv2d(256, 256, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.Conv2d(256, 256, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.MaxPool2d(2, 2),
+            #256 28 16
+            nn.Conv2d(256, 512, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.Conv2d(512, 512, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.Conv2d(512, 512, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.MaxPool2d(2, 2),
+            #512 14 8
+            nn.Conv2d(512, 512, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.Conv2d(512, 512, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.Conv2d(512, 512, 3, padding=1),nn.LeakyReLU(0.2),
+            nn.MaxPool2d(2, 2)
+        )
+        #512 7 4
+
+        self.avg_pool = nn.AvgPool2d(7)
+        #512 1 1
+        self.classifier = nn.Linear(512, 1000)
+        """
+        self.fc1 = nn.Linear(512*2*2,4096)
+        self.fc2 = nn.Linear(4096,4096)
+        self.fc3 = nn.Linear(4096,10)
+        """
+
+    def forward(self, x):
+        #print(x.size())
+        features = self.conv(x)
+        #print(features.size())
+        x = self.avg_pool(features)
+        #print(avg_pool.size())
+        x = x.view(features.size(0), -1)
+        #print(flatten.size())
+        x = self.classifier(x)
+        #x = self.softmax(x)
+        return x, features
